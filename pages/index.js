e0d2f5721977c7bf951f86315ac8631fc323f5bf
Afix Vega Praditya <afixv00@gmail.com>
2023-09-01 18:16:46 +0700

    feat: add hero

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title key="pagetitle">Afix Vega</title>
-      <meta name='description' content='Afix Vega - Welcome to My Portfolio' key='metadescription' />
-      <link rel='icon' href='/afixicon.svg' />
-    </Head>
-    <main>
-      <h1>Welcome to My Personal Website</h1>
-    </main>
+      <Head>
+        <title key="pagetitle">Afix Vega</title>
+        <meta name="description" content="Afix Vega - Welcome to My Portfolio" key="metadescription" />
+        <link rel="icon" href="/afixicon.svg" />
+      </Head>
+      <main>
+        <div className="flex justify-center items-center flex-col">
+          <Image className="rounded-full" alt="Afix's Profile Picture" src="/mypicture.png" width="150" height="150" />
+          <h2 className="font-semibold text-4xl">Hi, I am Afix 👋</h2>
+          <p className="text-c-body ">Information Engineering Undergraduate Student</p>
+          <h1 className="font-semibold text-[40px] text-center heigh leading-tight mt-6">
+            Explore the future with <br />
+            captivating UI/UX design and <br />
+            front-end abilities.
+          </h1>
+        </div>
+      </main>
     </>
-  )
+  );
 }
