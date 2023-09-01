e5f07ec9568ce7a9a9f10cf9d6f37cd8e3f2be3b
Afix Vega Praditya <afixv00@gmail.com>
2023-09-01 18:16:21 +0700

    feat: add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Work_Sans } from "next/font/google";
+
+const worksans = Work_Sans({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={worksans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
