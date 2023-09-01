f68996c179e4c782e38ecba605f2a53d4fd57a58
Afix Vega Praditya <afixv00@gmail.com>
2023-09-01 18:17:45 +0700

    fix: formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'primary' : '#1A1A1A',
-        'secondary' : '#595E70',
-        'black-grey' : '#2D2D2D',
-        'body' : '#B7B7B7',
-        'body-fade' : '#898989',
+        "c-primary": "#1A1A1A",
+        "c-secondary": "#595E70",
+        "c-black-grey": "#2D2D2D",
+        "c-body": "#B7B7B7",
+        "c-body-fade": "#898989",
       },
     },
   },
   plugins: [],
-}
+};
