4bd72dbd8d6d464a169da14eae80407aa8f18ebd
Afix Vega Praditya <afixv00@gmail.com>
2023-09-01 09:36:19 +0700

    feat: add color system

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary' : '#1A1A1A',
+        'secondary' : '#595E70',
+        'black-grey' : '#2D2D2D',
+        'body' : '#B7B7B7',
+        'body-fade' : '#898989',
+      },
     },
   },
   plugins: [],
