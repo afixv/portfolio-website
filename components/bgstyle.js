7a387247f5d1aa6cfd8d6d34decf9d4f63ab1b42
Afix Vega Praditya <afixv00@gmail.com>
2023-09-02 11:33:09 +0700

    feat: add component for bg

---
+++ components/bgstyle.js
@@ -0,0 +1,13 @@
+export default function BgStyle() {
+    return (
+      <div className="absolute top-0 left-0 right-0 bottom-0 -z-50">
+        <div className="mx-auto max-w-[850px]">
+          <div className="flex justify-between flex-row">
+            {Array.from({ length: 5 }, (_, index) => (
+              <div key={index} className="w-[.5px] md:w-[1px] bg-c-black-grey h-screen"></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
