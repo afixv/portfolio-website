06051eca956dfbe15915e0f1c49cfb74335ede99
Afix Vega Praditya <afixv00@gmail.com>
2023-09-02 20:08:48 +0700

    fix:website title

--- pages/_app.js
+++ pages/_app.js
@@ -3,20 +3,32 @@ import BgStyle from "@/components/bgstyle";
 import Navbar from "@/components/navbar";
 import BackTop from "@/components/backTop";
 import { Work_Sans } from "next/font/google";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 const worksans = Work_Sans({
   subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
+  weight: ["400", "500", "600", "700"],
   display: "swap",
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const routerName = router.route !== "/" ? router.route.charAt(1).toUpperCase() + router.route.slice(2) : "Home";
+  const pageTitle = `Afix Vega - ${routerName}`;
   return (
-    <main className={worksans.className}>
-      <BgStyle />
-      <Navbar />
-      <BackTop />
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title key="pagetitle">{pageTitle}</title>
+        <meta name="description" content="Afix Vega - Welcome to My Portfolio" key="metadescription" />
+        <link rel="icon" href="/afixicon.svg" />
+      </Head>
+      <main className={worksans.className}>
+        <BgStyle />
+        <Navbar />
+        <BackTop />
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
