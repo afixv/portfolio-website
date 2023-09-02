4d528e6e132ad195a64ef6f6dde1101a90ed89fe
Afix Vega Praditya <afixv00@gmail.com>
2023-09-02 13:43:34 +0700

    style: formatting

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,28 @@ export default function Home() {
       </Head>
       <div className="min-h-screen">
         <main>
-          <div className="mt-10 flex min-h-[50vh] justify-center items-center flex-col">
-            <Image className="rounded-full" alt="Afix's Profile Picture" src="/mypicture.png" width="150" height="150" />
-            <h2 className="font-semibold text-4xl">Hi, I am Afix 👋</h2>
-            <p className="text-c-body ">Information Engineering Undergraduate Student</p>
-            <h1 className="font-semibold text-[40px] text-center heigh leading-tight mt-6">
-              Explore the future with <br />
-              captivating UI/UX design and <br />
-              front-end abilities.
-            </h1>
-            <Link href="#">
-              <Image className="mt-20 animate-bounce pointer-events-none" alt="arrow" src="/arrowdown.svg" width="18" height="18" />
-            </Link>
-            <Image className="absolute ml-[200px] mt-[300px] pointer-events-none" src='/scratch.svg' alt='scratch style' width='200' height='200' />
-          </div>
+          <Hero />
         </main>
       </div>
     </>
   );
 }
+
+const Hero = () => {
+  return (
+    <div className="mt-10 flex min-h-[50vh] justify-center items-center flex-col">
+      <Image className="rounded-full" alt="Afix's Profile Picture" src="/mypicture.png" width="150" height="150" />
+      <h2 className="font-semibold text-4xl">Hi, I am Afix 👋</h2>
+      <p className="text-c-body ">Information Engineering Undergraduate Student</p>
+      <h1 className="font-semibold text-[40px] text-center heigh leading-tight mt-6">
+        Explore the future with <br />
+        captivating UI/UX design and <br />
+        front-end abilities.
+      </h1>
+      <Link href="#">
+        <Image className="mt-20 animate-bounce pointer-events-none" alt="arrow" src="/arrowdown.svg" width="18" height="18" />
+      </Link>
+      <Image className="absolute ml-[200px] mt-[300px] pointer-events-none" src="/scratch.svg" alt="scratch style" width="200" height="200" />
+    </div>
+  );
+};
