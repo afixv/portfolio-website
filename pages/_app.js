75eba813ebf02979564915f388aa54fcfcfe3d07
Afix Vega Praditya <afixv00@gmail.com>
2023-09-02 11:33:29 +0700

    feat: add component to page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import BgStyle from "@/components/bgstyle";
+import Navbar from "@/components/navbar";
 import { Work_Sans } from "next/font/google";
 
 const worksans = Work_Sans({
@@ -10,6 +12,8 @@ const worksans = Work_Sans({
 export default function App({ Component, pageProps }) {
   return (
     <main className={worksans.className}>
+      <BgStyle />
+      <Navbar />
       <Component {...pageProps} />
     </main>
   );
