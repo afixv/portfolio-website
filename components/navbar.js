597e836dcb0fb375d106b94c18239c306dabe82b
Afix Vega Praditya <afixv00@gmail.com>
2023-09-02 20:13:08 +0700

    feat: add navbar hide on scroll

--- components/navbar.js
+++ components/navbar.js
@@ -2,11 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { BsMedium, BsLinkedin, BsGithub } from "react-icons/bs";
+import { useEffect } from "react";
 
 export default function Navbar() {
   const router = useRouter();
-
   const isLinkActive = (href) => router.pathname === href;
+
+  useEffect(() => {
+    let scrollPosition = 0;
+    const isScrollingDown = () => {
+      let scrollingDown = false;
+      let newScrollPosition = window.pageYOffset;
+      if (newScrollPosition > scrollPosition) {
+        scrollingDown = true;
+      }
+      scrollPosition = newScrollPosition;
+      return scrollingDown;
+    };
+    const handleScroll = () => {
+      const navbar = document.querySelector("[data-navbarDisapear]");
+      if (isScrollingDown() || window.pageYOffset < 50) {
+        navbar.classList.add("top-6", "visible");
+        navbar.classList.remove("-top-20", "invisible");
+      } else {
+        navbar.classList.remove("top-6", "visible");
+        navbar.classList.add("-top-20", "invisible");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+  });
+
   return (
     <>
       <div className="flex justify-between mx-auto container max-w-[1120px] mt-8 z-10 px-10">
@@ -33,7 +58,7 @@ export default function Navbar() {
           </ul>
         </div>
       </div>
-      <nav className="-mt-10 mx-auto text-c-body-fade  sticky top-6 bg-c-black-grey py-2.5 px-6 rounded-full drop-shadow-lg w-[420px] z-10">
+      <nav data-navbarDisapear className="-mt-10 mx-auto text-c-body-fade sticky top-6 bg-c-black-grey py-2.5 px-6 rounded-full drop-shadow-lg w-[420px] z-10 transition-all duration-500">
         <ul className="flex justify-center [&>li>a]:px-6 [&>li:hover]:text-white font-medium [&>li>a]:transition-all">
           <li>
             <Link href="/" className={isLinkActive("/") ? "text-white" : ""}>
