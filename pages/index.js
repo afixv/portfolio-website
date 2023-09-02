0a00c93cece524b60e68e2a4ae0ae5b63d988444
Afix Vega Praditya <afixv00@gmail.com>
2023-09-02 11:34:14 +0700

    fix: min height

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,20 @@ export default function Home() {
         <meta name="description" content="Afix Vega - Welcome to My Portfolio" key="metadescription" />
         <link rel="icon" href="/afixicon.svg" />
       </Head>
-      <main>
-        <div className="flex justify-center items-center flex-col">
-          <Image className="rounded-full" alt="Afix's Profile Picture" src="/mypicture.png" width="150" height="150" />
-          <h2 className="font-semibold text-4xl">Hi, I am Afix 👋</h2>
-          <p className="text-c-body ">Information Engineering Undergraduate Student</p>
-          <h1 className="font-semibold text-[40px] text-center heigh leading-tight mt-6">
-            Explore the future with <br />
-            captivating UI/UX design and <br />
-            front-end abilities.
-          </h1>
-        </div>
-      </main>
+      <div className="min-h-screen relative">
+        <main>
+          <div className="flex justify-center items-center flex-col">
+            <Image className="rounded-full" alt="Afix's Profile Picture" src="/mypicture.png" width="150" height="150" />
+            <h2 className="font-semibold text-4xl">Hi, I am Afix 👋</h2>
+            <p className="text-c-body ">Information Engineering Undergraduate Student</p>
+            <h1 className="font-semibold text-[40px] text-center heigh leading-tight mt-6">
+              Explore the future with <br />
+              captivating UI/UX design and <br />
+              front-end abilities.
+            </h1>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
