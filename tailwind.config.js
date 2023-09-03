3a0f9d375b06e86ac3e7fc44b51c67a2ecdda16a
Afix Vega Praditya <afixv00@gmail.com>
2023-09-03 15:29:29 +0700

    feat: add waving animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,23 @@ module.exports = {
       colors: {
         "c-primary": "#1A1A1A",
         "c-secondary": "#595E70",
-        "c-secondary-fade" : 'rgba(89, 94, 112, 0.25);',
+        "c-secondary-fade": "rgba(89, 94, 112, 0.25);",
         "c-black-grey": "#2D2D2D",
         "c-body": "#B7B7B7",
         "c-body-fade": "#898989",
-        "c-button": '#F5F5F5'
+        "c-button": "#F5F5F5",
+      },
+      keyframes: {
+        waving: {
+          "0%, 70%, 100% ": { transform: "rotate(0deg)" },
+          "15%, 40% ": { transform: "rotate(14deg)" },
+          "30%": { transform: "rotate(-8deg)" },
+          "50%": { transform: "rotate(-4deg)" },
+          "60%": { transform: "rotate(10deg)" },
+        },
+      },
+      animation: {
+        waving: "waving 2s ease-in-out infinite",
       },
     },
   },
