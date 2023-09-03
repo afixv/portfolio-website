04b507421aa929916aecea38420f9bec1a4cdb05
Afix Vega Praditya <afixv00@gmail.com>
2023-09-03 15:55:05 +0700

    fix: card blog style

--- components/cardBlog.js
+++ components/cardBlog.js
@@ -1,21 +1,31 @@
 import Image from "next/image";
-import { Button } from "./button";
 import Link from "next/link";
+import { FaRegEye } from "react-icons/fa";
+import {AiOutlineComment} from "react-icons/ai"
 
 export default function CardBlog({ className }) {
   return (
     <>
       <div className={`${className} backdrop-blur-sm rounded-xl w-full min-h-[16rem] md:w-[30%] bgCard hover:-translate-y-1.5  hover:opacity-80 transition-all`}>
         <div className="relative h-3/5">
-          <Image className="rounded-t-xl" src={"/180dc.png"} alt="Project 180dc UGM Website" fill style={{ objectFit: "cover" }} />
+          <Image className="rounded-t-xl" src={"/blog.png"} alt="Project 180dc UGM Website" fill style={{ objectFit: "cover" }} />
         </div>
-        <Link href="/" className="relative flex flex-col px-4 pt-2 pb-4">
+        <Link href="/" className="relative flex flex-col px-4 pt-4 pb-4">
           <div className="flex">
-            <p className="text-base font-semibold">Project 818</p>
+            <p className="text-base leading-tight font-semibold">Understanding Next.js Data Fetching (CSR, SSR, SSG, ISR)</p>
             <div className="icon"></div>
           </div>
-          <p className="text-xs -mt-1">Role: Frontend Developer</p>
-          <p className=" text-xs mt-2 font-medium text-c-body leading-tight">Event tahunan Departemen Teknik Elektro dan Teknologi Informasi UGM</p>
+          <div className="flex flex-row items-center gap-2 mt-2">
+            <p className="text-xs">September 3, 2023</p>
+            <div className="text-xs gap-1 items-center flex flex-row">
+              {" "}
+              <FaRegEye /> 133{" "}
+            </div>
+            <div className="text-xs gap-1 items-center flex flex-row">
+              {" "}
+              <AiOutlineComment /> 11{" "}
+            </div>
+          </div>
         </Link>
       </div>
     </>
