5d19c9bc925b05f4dcf4c14384fc0cc9656c0e79
Afix Vega Praditya <afixv00@gmail.com>
2023-09-03 16:37:24 +0700

    fix: footer style

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,19 @@
 import { Button } from "./button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <>
-      <div className="mt-20 md:max-w-[1080px] mx-auto pt-32 px-10 pb-12 relative bottom-0 w-full bgFooter">
+      <div className="mt-20 md:max-w-[1080px] mx-auto pt-32 px-10 pb-16 relative bottom-0 w-full bgFooter">
+        <Image className="absolute right-2 bottom-16" alt="Icon Outlined" src={"/afixiconoutlined.png"} width={280} height={280}></Image>
         <div className="flex md:justify-start">
-          <div className="flex flex-col md:w-4/5 md:ml-7">
-            <h3 className="text-[24px] md:text-4xl">Don’t Be Shy - Say Hi!</h3>
+          <div className="flex flex-col  md:ml-7">
+            <h3 className="text-[24px] md:text-4xl">Dont Be Shy - Say Hi!</h3>
             <h2 className="text-4xl leading-[1.1] mt-1 md:text-[48px]">Do you have any project ideas? Feel free to get in touch! 😉</h2>
-            <p className="text-c-body text-xs md:text-base  font-normal mt-2">These are some of the projects I have created. These projects have not only helped me build my portfolio, but also enhanced the skills I have developed.</p>
+            <p className="text-c-body text-xs md:text-base  font-normal mt-2">
+              Prepare for liftoff, because Im bursting with enthusiasm to infuse your projects with a touch of enchantment! Together, lets rocket towards success and turn your ideas into something extraordinary! 🚀 🌟
+            </p>
             <Link href={"/contact"} className="mt-6">
               <Button size={"lg"}>Contact Me</Button>
             </Link>
