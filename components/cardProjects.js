f287fbeda4454e62d519b736a5ed38c5319d4525
afixv <afixv00@gmail.com>
2023-12-15 10:22:14 +0700

    feat: add project detail

--- components/cardProjects.js
+++ components/cardProjects.js
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CardProject({ className }) {
+export default function CardProject({ className, image, title, role, desc, link }) {
   return (
     <>
       <div className={`${className} backdrop-blur-sm flex flex-col rounded-xl w-full  md:w-[30%] bgCard hover:-translate-y-1.5  hover:opacity-80 transition-all`}>
         <div className="flex ">
-          <Image className="rounded-t-xl  w-full h-[160px] object-cover" src={"/180dc.png"} width="500" height={"500"} alt="Project 180dc UGM Website" />
+          <Image className="rounded-t-xl  w-full h-[160px] object-cover" src={image} width="500" height={"500"} alt={title} />
         </div>
-        <Link href="/" className=" flex flex-col px-4 pt-2 pb-4">
+        <Link href={link} target="_blank" className=" flex flex-col px-4 pt-2 pb-4">
           <div className="flex">
-            <p className="text-base font-semibold">Project 818</p>
+            <p className="text-base font-semibold">{title}</p>
             <div className="icon"></div>
           </div>
-          <p className="text-xs -mt-1">Role: Frontend Developer</p>
-          <p className=" text-xs mt-2 font-medium text-c-body leading-tight">Event tahunan Departemen Teknik Elektro dan Teknologi Informasi UGM</p>
+          <p className="text-xs -mt-1">Role: {role}</p>
+          <p className=" text-xs mt-2 font-medium text-c-body leading-tight">{desc}</p>
         </Link>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
         <About />
         <Projects />
         <Skills />
-        <Blog />
       </div>
     </>
   );
@@ -24,19 +23,48 @@ export default function Home() {
 const Hero = () => {
   return (
     <>
-      <div data-aos="fade-up" className="-mt-8 mx-5 flex min-h-screen justify-center items-center flex-col">
-        <Image className="rounded-full" alt="Afix's Profile Picture" src="/mypicture.png" width="150" height="150" />
+      <div
+        data-aos="fade-up"
+        className="-mt-8 mx-5 flex min-h-screen justify-center items-center flex-col">
+        <Image
+          className="rounded-full"
+          alt="Afix's Profile Picture"
+          src="/mypicture.png"
+          width="150"
+          height="150"
+        />
         <div className="flex items-center mx-auto">
-          <h2 className=" text-center font-semibold mt-3 text-4xl">Hi, I am Afix</h2>
-          <span className="text-center text-4xl animate-waving flex justify-center">👋</span>
+          <h2 className=" text-center font-semibold mt-3 text-4xl">
+            Hi, I am Afix
+          </h2>
+          <span className="text-center text-4xl animate-waving flex justify-center">
+            👋
+          </span>
         </div>
 
-        <p className="text-center text-body">Information Engineering Undergraduate Student</p>
-        <h1 className="font-semibold mx-10 text-[28px] md:text-[40px] max-w-[400px] md:max-w-[600px] text-center heigh leading-tight mt-6">Explore the future with captivating UI/UX design and front-end abilities.</h1>
+        <p className="text-center text-body">
+          Information Engineering Undergraduate Student
+        </p>
+        <h1 className="font-semibold mx-10 text-[28px] md:text-[40px] max-w-[400px] md:max-w-[600px] text-center heigh leading-tight mt-6">
+          Explore the future with captivating UI/UX design and front-end
+          abilities.
+        </h1>
         <Link href="#about">
-          <Image className="mt-20 animate-bounce pointer-events-none" alt="arrow" src="/arrowdown.svg" width="18" height="18" />
+          <Image
+            className="mt-20 animate-bounce pointer-events-none"
+            alt="arrow"
+            src="/arrowdown.svg"
+            width="18"
+            height="18"
+          />
         </Link>
-        <Image className="absolute w-40 md:w-[200px] ml-[10px] xs:ml-[100px] md:ml-[200px] mt-[300px] xs:mt-[230px] md:mt-[300px] pointer-events-none" src="/scratch.svg" alt="scratch style" width="200" height="200" />
+        <Image
+          className="absolute w-40 md:w-[200px] ml-[10px] xs:ml-[100px] md:ml-[200px] mt-[300px] xs:mt-[230px] md:mt-[300px] pointer-events-none"
+          src="/scratch.svg"
+          alt="scratch style"
+          width="200"
+          height="200"
+        />
       </div>
     </>
   );
@@ -45,12 +73,18 @@ const Hero = () => {
 const About = () => {
   return (
     <>
-      <div data-aos="fade-down" id="about" className="flex justify-center pt-20">
+      <div
+        data-aos="fade-down"
+        id="about"
+        className="flex justify-center pt-20">
         <div className="max-w-[800px] self-center flex flex-col mx-10 text-center">
           <h2 className="font-semibold">Hello, it’s me! 😉</h2>
           <p className="text-body text-justify md:text-center">
-            Uncover the astonishing IT aptitudes of a university student who is revolutionizing the UI/UX design and front-end development industries. This rising technology virtuoso specializes in crafting stylish, contemporary, and
-            immersive websites, converting common concepts into exceptional online encounters.
+            Uncover the astonishing IT aptitudes of a university student who is
+            revolutionizing the UI/UX design and front-end development
+            industries. This rising technology virtuoso specializes in crafting
+            stylish, contemporary, and immersive websites, converting common
+            concepts into exceptional online encounters.
           </p>
         </div>
       </div>
@@ -65,14 +99,38 @@ const Projects = () => {
         <div className="flex md:justify-end">
           <div data-aos="fade-left" className="flex flex-col md:w-2/3 md:mr-7">
             <h2 className="text-center md:text-right">🚀 Highlight Project </h2>
-            <p className=" text-desc text-center md:text-right">These are some of the projects I have created. These projects have not only helped me build my portfolio, but also enhanced the skills I have developed.</p>
+            <p className=" text-desc text-center md:text-right">
+              These are some of the projects I have created. These projects have
+              not only helped me build my portfolio, but also enhanced the
+              skills I have developed.
+            </p>
           </div>
         </div>
-        <div data-aos="fade-right" className="max-w-[400px] md:max-w-[1080px] mx-auto">
+        <div
+          data-aos="fade-right"
+          className="max-w-[400px] md:max-w-[1080px] mx-auto">
           <div className=" flex justify-center mt-10 flex-wrap gap-4">
-            <CardProject />
-            <CardProject />
-            <CardProject />
+            <CardProject
+              image="/rentalin.png"
+              title="Rentalin"
+              role="Full Stack Developer"
+              desc="Website untuk tugas mata kuliah Pemrograman Web yang dibuat menggunakan ExpressJS dan NextJS"
+              link="https://paw-kelompok-11-client.vercel.app/"
+            />
+            <CardProject
+              image="/180dc.png"
+              title="180DC UGM"
+              role="UI/UX Designer & Front End Developer"
+              desc="180 Degrees Consulting UGM is the branch of the worlds largest student-led consultancy for non-profits & social enterprises."
+              link="https://www.180dcugm.org/"
+            />
+            <CardProject
+              image="/old-portfolio.png"
+              title="My Old Portfolio Website"
+              role="Frontend Developer"
+              desc="Portfolio website saya yang lama menggunakan HTML, CSS, dan Javascript Vanilla."
+              link="https://www.180dcugm.org/"
+            />
           </div>
         </div>
 
@@ -101,13 +159,24 @@ const Skills = () => {
   ];
   return (
     <>
-      <div data-aos="fade-right" className="-z-10  bgSection md:rounded-r-[50px] h-[32rem] md:h-[30rem] absolute mt-20 w-full md:w-[80%]"></div>
-      <div data-aos="fade-right" className="flex-col px-10 max-w-[1080px] mx-auto mt-32">
+      <div
+        data-aos="fade-right"
+        className="-z-10  bgSection md:rounded-r-[50px] h-[32rem] md:h-[30rem] absolute mt-20 w-full md:w-[80%]"></div>
+      <div
+        data-aos="fade-right"
+        className="flex-col px-10 max-w-[1080px] mx-auto mt-32">
         <div className="flex flex-col md:ml-7">
           <h2>Specialized Skills 👨‍💻 </h2>
-          <p className="text-desc sm:w-2/3">These are some of the projects I have created. These projects have not only helped me build my portfolio, but also enhanced the skills I have developed.</p>
+          <p className="text-desc sm:w-2/3">
+            These are some of the projects I have created. These projects have
+            not only helped me build my portfolio, but also enhanced the skills
+            I have developed.
+          </p>
         </div>
-        <div data-aos="zoom-in" data-aos-delay="200" className="mt-10 max-w-[400px] md:max-w-[700px] mx-auto">
+        <div
+          data-aos="zoom-in"
+          data-aos-delay="200"
+          className="mt-10 max-w-[400px] md:max-w-[700px] mx-auto">
           <div className=" flex justify-center mt-6 flex-wrap gap-4 md:gap-12">
             {iconData.map((data) => (
               <CardIcon key={data.id} color={data.color} icon={data.icon} />
@@ -126,11 +195,18 @@ const Blog = () => {
         <div data-aos="fade-down" className="flex md:justify-center">
           <div className="flex flex-col md:w-2/3 md:mr-7">
             <h2 className="text-center">✏️ Latest Blog </h2>
-            <p className=" text-desc text-center">These are some of the projects I have created. These projects have not only helped me build my portfolio, but also enhanced the skills I have developed.</p>
+            <p className=" text-desc text-center">
+              These are some of the projects I have created. These projects have
+              not only helped me build my portfolio, but also enhanced the
+              skills I have developed.
+            </p>
           </div>
         </div>
-        <div data-aos="fade-right" data-aos-delay="200" className="max-w-[400px] md:max-w-[1080px] mx-auto">
-          <div  className=" flex justify-center mt-10 flex-wrap gap-4">
+        <div
+          data-aos="fade-right"
+          data-aos-delay="200"
+          className="max-w-[400px] md:max-w-[1080px] mx-auto">
+          <div className=" flex justify-center mt-10 flex-wrap gap-4">
             <CardBlog />
             <CardBlog />
             <CardBlog />
