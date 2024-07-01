bfff348eb7ffe486a922573f41c23353a2445324
afixv <afixv00@gmail.com>
2024-07-01 23:26:51 +0700

    fix: adjust useEffect in bgstyle

--- components/bgstyle.js
+++ components/bgstyle.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 
-export default function BgStyle( {pageID} ) {
+export default function BgStyle({ pageID }) {
   const [mainContentHeight, setMainContentHeight] = useState("100vh");
 
   useEffect(() => {
     const mainContent = document.getElementById(pageID);
-    const height = mainContent ? mainContent.scrollHeight + 300 + "px" : "100vh";
+    const height = mainContent
+      ? mainContent.scrollHeight + 300 + "px"
+      : "100vh";
     setMainContentHeight(height);
-  }, []);
+  }, [pageID]);
 
   return (
     <div className="absolute inset-0 -z-50">
       <div className="mx-auto max-w-[850px]">
         <div className="flex justify-between flex-row">
           {Array.from({ length: 5 }, (_, index) => (
-            <div style={{ height: mainContentHeight }} key={index} className="w-[.5px] md:w-[1px] bg-c-black-grey"></div>
+            <div
+              style={{ height: mainContentHeight }}
+              key={index}
+              className="w-[.5px] md:w-[1px] bg-c-black-grey"></div>
           ))}
         </div>
       </div>
