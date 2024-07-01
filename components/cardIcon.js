2ab935d91767d9dc640197e131d03776f28bcbd3
afixv <afixv00@gmail.com>
2024-07-02 00:38:31 +0700

    feat: add tooltip to icon

--- components/cardIcon.js
+++ components/cardIcon.js
@@ -1,14 +1,5 @@
-import { CgFigma } from "react-icons/cg";
-import { SiAdobeillustrator, SiAdobephotoshop } from "react-icons/si";
-import { RiJavascriptFill } from "react-icons/ri";
-import { TbBrandCpp, TbBrandNextjs } from "react-icons/tb";
-import {
-  BiLogoTailwindCss,
-  BiLogoPostgresql,
-  BiLogoReact,
-} from "react-icons/bi";
-import { AiOutlineAntDesign } from "react-icons/ai";
 import Image from "next/image";
+import { Tooltip } from "react-tooltip";
 
 export default function CardIcon({ color, icon }) {
   const iconComponents = {
@@ -26,16 +17,35 @@ export default function CardIcon({ color, icon }) {
     Python: "/icons-brand/python.svg",
   };
 
+  const iconName = {
+    Figma: "Figma",
+    Photoshop: "Photoshop",
+    AI: "Adobe Illustrator",
+    Js: "JavaScript",
+    Cpp: "C++",
+    ReactBrand: "React / React Native",
+    NextJs: "NextJs",
+    Tailwind: "Tailwind",
+    Antd: "Ant Design",
+    Pg: "PostgreSQL",
+    Laravel: "Laravel",
+    Python: "Python",
+  };
+
   const selectedIcon = iconComponents[icon || "/icons/figma.svg"];
   const selectedColor = [color || "text-white"];
   return (
     <>
       <div
+        data-tooltip-content={iconName[icon]}
+        data-tip={icon}
+        data-tooltip-id={icon}
         className={` ${selectedColor} bgCard backdrop-blur-sm rounded-xl py-3 px-3 sm:py-4 sm:px-4 sm:text-5xl hover:-translate-y-1.5  hover:opacity-80 transition-all`}>
         <div className="pointer-events-none">
           <Image src={selectedIcon} alt="icon" width={48} height={48} />
         </div>
       </div>
+      <Tooltip id={icon} style={{ fontSize: 12, borderRadius: 8 }} />
     </>
   );
 }
