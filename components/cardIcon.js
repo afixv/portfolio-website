aaab7c46cb5bec3492cb3a2c28dedc4fa80b31c5
afixv <afixv00@gmail.com>
2024-07-01 23:27:36 +0700

    feat: register some icon

--- components/cardIcon.js
+++ components/cardIcon.js
@@ -2,7 +2,11 @@ import { CgFigma } from "react-icons/cg";
 import { SiAdobeillustrator, SiAdobephotoshop } from "react-icons/si";
 import { RiJavascriptFill } from "react-icons/ri";
 import { TbBrandCpp, TbBrandNextjs } from "react-icons/tb";
-import { BiLogoTailwindCss, BiLogoPostgresql, BiLogoReact } from "react-icons/bi";
+import {
+  BiLogoTailwindCss,
+  BiLogoPostgresql,
+  BiLogoReact,
+} from "react-icons/bi";
 import { AiOutlineAntDesign } from "react-icons/ai";
 import Image from "next/image";
 
@@ -18,15 +22,18 @@ export default function CardIcon({ color, icon }) {
     Tailwind: "/icons-brand/tw.svg",
     Antd: "/icons-brand/antd.svg",
     Pg: "/icons-brand/pgsql.svg",
+    Laravel: "/icons-brand/laravel.svg",
+    Python: "/icons-brand/python.svg",
   };
 
   const selectedIcon = iconComponents[icon || "/icons/figma.svg"];
   const selectedColor = [color || "text-white"];
   return (
     <>
-      <div className={` ${selectedColor} bgCard backdrop-blur-sm rounded-xl py-3 px-3 sm:py-4 sm:px-4 sm:text-5xl hover:-translate-y-1.5  hover:opacity-80 transition-all`}>
+      <div
+        className={` ${selectedColor} bgCard backdrop-blur-sm rounded-xl py-3 px-3 sm:py-4 sm:px-4 sm:text-5xl hover:-translate-y-1.5  hover:opacity-80 transition-all`}>
         <div className="pointer-events-none">
-          <Image src={selectedIcon} alt="icon" width={48} height={48}/>
+          <Image src={selectedIcon} alt="icon" width={48} height={48} />
         </div>
       </div>
     </>
