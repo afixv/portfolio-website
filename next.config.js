c529653845fe55886cf03b00a4574fba2329b805
afixv <afixv00@gmail.com>
2025-09-21 17:14:41 +0700

    fix: update redirects configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://afix.works/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
